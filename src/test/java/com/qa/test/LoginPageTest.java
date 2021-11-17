58defeba95bb621be6df5dc3166bfe20dc874560
Shivali Tandon <shivali288@gmail.com>
2021-11-17 12:17:41 -0500

    first git commit for my selenium code

---
+++ src/test/java/com/qa/test/LoginPageTest.java
@@ -0,0 +1,12 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class LoginPageTest {
+	
+	@Test
+	public void loginPageTest() {
+		System.out.println("login page test");
+	}
+
+}
